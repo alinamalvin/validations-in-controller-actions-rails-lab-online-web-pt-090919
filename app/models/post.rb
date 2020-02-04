2b1ada3ce80a1b5529aa028dba79cb3e0a79ca7b
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-04 03:05:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :title
-  validates :category, inclusion: { in: %w("Fiction", "Non-Fiction")}
-  validates :content, length: { minimum: 100}
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
