470c990d392e081f6ae32ea02822ffbee0198e5a
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-04 03:02:19 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,7 @@ class AuthorsController < ApplicationController
    @author = Author.new(author_params)
     if @author.valid?
       @author.save
-    redirect_to author_path(@author)	    r
+    redirect_to author_path(@author)	    
   else
     render :new
   end
